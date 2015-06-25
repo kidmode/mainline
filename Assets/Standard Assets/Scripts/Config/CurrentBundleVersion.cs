96308c0583465dd0f9dab292519d6900484915ed
seanphoenix <seanphoenixx@gmail.com>
2015-06-25 18:48:51 +0800

    * uploaded Kid_Mode_06_25_2015_prod_v6.0.5075.apk

--- Assets/Standard Assets/Scripts/Config/CurrentBundleVersion.cs
+++ Assets/Standard Assets/Scripts/Config/CurrentBundleVersion.cs
@@ -1,9 +1,5 @@
 public static class CurrentBundleVersion
 {
-<<<<<<< HEAD
-	public static readonly string version = "6.0.5071";
-=======
-	public static readonly string version = "6.0.5073";
->>>>>>> 3aaa44a7380f1fa8f7276be6f44eda916f28cdb2
+	public static readonly string version = "6.0.5074";
 }
 
