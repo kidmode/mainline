d8eb980b0d6e00cbca2df889517bcfedb3852c61
seanphoenix <seanphoenixx@gmail.com>
2015-07-31 11:42:38 +0800

    * build settings

--- Assets/Scripts/Game/Game.cs
+++ Assets/Scripts/Game/Game.cs
@@ -124,7 +124,7 @@ public class Game : MonoBehaviour
 	{
 		// Sean: vzw
 		#if UNITY_ANDROID && !UNITY_EDITOR
-		Application.targetFrameRate = 30;
+		Application.targetFrameRate = 60;
 		
 		QualitySettings.vSyncCount = 0; 
 		
