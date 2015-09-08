45024cadfa73b92595266f0a8e12fcbe2398d0bd
seanphoenix <seanphoenixx@gmail.com>
2015-09-08 18:21:21 +0800

    * async load map

---
+++ Assets/Scripts/AsyncScreenLoader.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public delegate void RequestFinishedHandler(object obj);
+
+public class AsyncScreenLoader : MonoBehaviour {
+
+	static public AsyncScreenLoader instance;
+	// Use this for initialization
+	void Start () {
+		instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void LoadScreen(string resPath, RequestFinishedHandler handler) {
+		DebugUtils.Assert(handler != null);
+		StartCoroutine(_loadScreen(resPath, handler));
+	}
+
+	IEnumerator _loadScreen(string resPath, RequestFinishedHandler handler) {
+
+		ResourceRequest rr = Resources.LoadAsync<GameObject>(resPath);
+
+		yield return rr;
+
+		handler(rr.asset);
+	}
+}
---
+++ Assets/Scripts/LoadingIconAnimation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadingIconAnimation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		elapsedTime += Time.deltaTime;
+		float scale = 1.5f + Mathf.Sin(elapsedTime) * 0.5f;
+		Vector3 vec = new Vector3(scale, scale, scale);
+		transform.localScale = vec;
+	}
+
+	private float elapsedTime = 0.0f;
+}
