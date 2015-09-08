6ac869e2d6fa4327ea6880727d28cdd8baf0e5f9
Kevin Sang <kevin@uncagedgaming.com>
2015-09-08 16:40:57 +0800

    Remove gems UI

---
+++ Assets/Scripts/UI/Misc/ConstantDisable.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConstantDisable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(gameObject.activeSelf){
+
+			gameObject.SetActive(false);
+
+		}
+	
+	}
+}
