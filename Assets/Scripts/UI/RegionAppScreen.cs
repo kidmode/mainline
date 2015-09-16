9b773189c63aa372048a731e405b90284762fc03
Kevin Sang <kevin@uncagedgaming.com>
2015-09-16 16:02:59 +0800

    speechbubble

---
+++ Assets/Scripts/UI/RegionAppScreen.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RegionAppScreen : MonoBehaviour {
+
+	public GameObject SpeechBoxHey;
+
+	public GameObject SpeechBoxRecommendApps;
+
+	public int ShowHeyChance = 50;
+
+	public float ShowHeyTime = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+
+		InvokeRepeating("checkSpeechBoxHey", ShowHeyTime, ShowHeyTime);
+
+		SpeechBoxHey.SetActive(false);
+
+		SpeechBoxRecommendApps.SetActive(false);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void checkSpeechBoxHey(){
+
+		int rndSpeech = Random.Range(0, 100);
+
+		if(ShowHeyChance > rndSpeech){
+
+			SpeechBoxHey.SetActive(true);
+
+		}
+
+	}
+
+
+}
