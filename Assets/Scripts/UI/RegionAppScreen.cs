d5ca7847d5edf5042c17948a9518fe95ad1a8933
Kevin Sang <kevin@uncagedgaming.com>
2015-09-16 19:17:17 +0800

    flash when clicking monkey

--- Assets/Scripts/UI/RegionAppScreen.cs
+++ Assets/Scripts/UI/RegionAppScreen.cs
@@ -7,6 +7,10 @@ public class RegionAppScreen : MonoBehaviour {
 
 	public GameObject SpeechBoxRecommendApps;
 
+	public GameObject MonkeyImage;
+
+	public GameObject MonkeyImageHighlight;
+
 	public int ShowHeyChance = 50;
 
 	public float ShowHeyTime = 3.0f;
@@ -14,7 +18,8 @@ public class RegionAppScreen : MonoBehaviour {
 	public enum State{
 		Wating,
 		SpeechHey,
-		SpeechRecommend
+		SpeechRecommend,
+		ToParentGate
 	}
 
 	public State state;
@@ -27,6 +32,10 @@ public class RegionAppScreen : MonoBehaviour {
 		SpeechBoxHey.SetActive(false);
 
 		SpeechBoxRecommendApps.SetActive(false);
+
+		MonkeyImageHighlight.SetActive(false);
+
+		MonkeyImage.SetActive(true);
 	
 	}
 	
@@ -68,9 +77,23 @@ public class RegionAppScreen : MonoBehaviour {
 
 		}else if(state == State.SpeechRecommend){
 
+			MonkeyImageHighlight.SetActive(true);
+			
+			MonkeyImage.SetActive(false);
 
+			Invoke("parentGateThenRecommendedApps", 0.4f);
+
+//			parentGateThenRecommendedApps();
 
 		}
+
+	}
+
+	private void parentGateThenRecommendedApps(){
+
+		Game game = GameObject.FindWithTag("GameController").GetComponent<Game>();
+
+		game.callParentGate();
 
 	}
 
