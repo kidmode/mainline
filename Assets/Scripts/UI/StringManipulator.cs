7c33d2e109102a2a4c2d303e28f46c63f427834b
Kevin Sang <kevin@uncagedgaming.com>
2015-11-20 16:04:41 +0800

    shortened app name if they are too long

---
+++ Assets/Scripts/UI/StringManipulator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StringManipulator : MonoBehaviour {
+
+	public static int maxLength = 10; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	public static string getShortenOneLineAppName(string appName){
+
+		if(appName.Length > maxLength){
+
+			string returnName = appName.Substring(0, maxLength - 3) + " . . .";
+
+			return returnName;
+
+		}else {
+
+			return appName;
+
+		}
+
+	}
+}
