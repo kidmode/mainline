14d8fa148716f4c0fbf05142781082b37cc45ccb
Kevin Sang <kevin@uncagedgaming.com>
2015-12-18 11:42:51 +0800

    Created WWW downloader script to handle WWW requests Will use this for image loading for the featured spot

---
+++ Assets/Scripts/WWW/WWWDownloader.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class WWWDownloader : MonoBehaviour {
+
+	public Texture2D loadedTexture;
+
+	public string downloadURL;
+
+	public WWW www;
+
+	public static event Action OnDownloadDone;
+
+	public enum State{
+		START,
+		DONE
+	}
+
+	public State state;
+
+	// Use this for initialization
+	void Start () {
+
+		StartCoroutine(DownloadImage(downloadURL));
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private IEnumerator DownloadImage(string downloadURL)
+	{
+
+		state = State.START;
+
+		www = new WWW(downloadURL);
+
+//		Debug.Log(" start download ing ");
+
+		yield return www;
+
+//		Debug.Log(" www returned " + www.error);
+//		
+		if (www.error == null)
+		{
+
+			state = State.DONE;
+
+			if(OnDownloadDone != null)
+				OnDownloadDone();
+
+		}
+
+
+	}
+}
