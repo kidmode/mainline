6037998d27bbef8305d0ed37460465d9071f6c8e
Kevin Sang <kevin@uncagedgaming.com>
2016-02-05 16:52:19 +0800

    new component for localisation of Text and Screen controls - page witch controls

---
+++ Assets/Scripts/UI/Comp/LocalizeUnityUIText.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+
+public class LocalizeUnityUIText : MonoBehaviour {
+
+	public LocalizationProcess[] localizeProcesses;
+
+	// Use this for initialization
+	void Start () {
+
+		updateLocalText();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void updateLocalText(){
+
+		//
+		for (int i = 0; i < localizeProcesses.Length; i++) {
+
+			LocalizationProcess process = localizeProcesses[i];
+
+			process.uText.text = Localization.getString(process.localizeTag);
+
+		}
+
+	}
+
+}
+
+[Serializable]
+public class LocalizationProcess{
+
+	public Text uText;
+
+	public string localizeTag;
+
+}
